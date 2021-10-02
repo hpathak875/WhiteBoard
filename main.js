c83de89e3b5f65be742524f653fc852d74905581
Harshit Pathak <hpathak875@gmail.com>
2021-10-02 14:17:21 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const electron=require("electron");
+
+
+const app=electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+
+function createWindow(){
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration:true //enable node in this app
+          }
+      })
+
+      mainWindow.loadFile("public/index.html").then( function(){
+          mainWindow.webContents.openDevTools();
+          mainWindow.maximize();
+      });
+}
+
+
+app.whenReady().then( function(){
+    createWindow();
+});
+
